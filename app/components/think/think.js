b4443cb14be79e7838c85fc0b013d5acaceca813
Oliur Rahman <oliurr941@gmail.com>
2023-07-05 18:02:32 +0600

    fix images

--- app/components/think/think.js
+++ app/components/think/think.js
@@ -7,162 +7,139 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const Think = () => {
-    useEffect(() => {
-        const innerContents = document.querySelectorAll(".other__fade-in");
+  useEffect(() => {
+    const innerContents = document.querySelectorAll(".other__fade-in");
 
-        gsap.to(".other__hero-area", { opacity: 1, duration: 3 });
+    gsap.to(".other__hero-area", { opacity: 1, duration: 3 });
 
-        innerContents.forEach((content) => {
-            const innerElements = content.querySelectorAll(
-                "h1, h2, h3, h4, h5, p, .single__content-image, .think__item-image"
-            );
+    innerContents.forEach((content) => {
+      const innerElements = content.querySelectorAll(
+        "h1, h2, h3, h4, h5, p, .single__content-image, .think__item-image"
+      );
 
-            innerElements.forEach((element) => {
-                gsap.from(element, {
-                    opacity: 0,
-                    filter: "blur(6px)",
-                    stagger: 0.25,
-                    ease: "power2.out",
-                    duration: 0.8,
-                    scrollTrigger: {
-                        trigger: element,
-                        start: "top 80%",
-                        toggleActions: "play none none none",
-                    },
-                });
-            });
+      innerElements.forEach((element) => {
+        gsap.from(element, {
+          opacity: 0,
+          filter: "blur(6px)",
+          stagger: 0.25,
+          ease: "power2.out",
+          duration: 0.8,
+          scrollTrigger: {
+            trigger: element,
+            start: "top 80%",
+            toggleActions: "play none none none",
+          },
         });
-    }, []);
+      });
+    });
+  }, []);
 
-    return (
-        <section className="other__hero-area">
-            <div className="other__hero-bg"></div>
-            <div className="other__hero-bg other__white-bg"></div>
+  return (
+    <section className="other__hero-area">
+      <div className="other__hero-bg"></div>
+      <div className="other__hero-bg other__white-bg"></div>
 
-            <div className="container">
-                <div className="other__hero">
-                    {/* Single Content */}
-                    <div className="single__content other__fade-in">
-                        <h1>How We Think</h1>
+      <div className="container">
+        <div className="other__hero">
+          {/* Single Content */}
+          <div className="single__content other__fade-in">
+            <h1>How We Think</h1>
 
-                        <div className="single__content-subhead">
-                            <p>
-                                ThunderHouse focuses on Innovation because
-                                Innovation fuels growth…and significant
-                                innovations fuel significant, sustainable
-                                growth.
-                            </p>
-                        </div>
-
-                        <div className="glossy__heading glossy__bg_quote">
-                            <p>
-                                Innovation (ɪnəˈveɪʃən/) n. A better way to
-                                satisfy needs and wants
-                            </p>
-                        </div>
-
-                        <div className="think__items">
-                            {/* Single Item */}
-                            <div className="think__item">
-                                <div className="think__item-content single__content-subhead">
-                                    <h3>What is Innovation?</h3>
-                                    <p>
-                                        The very structure of established
-                                        organizations, the operations, systems
-                                        and procedures, that keep companies on
-                                        track today become barriers to
-                                        innovations that fuel the growth of
-                                        these businesses tomorrow. The
-                                        "Innovator's Dilemma," as Harvard
-                                        professor Clayton Christensen pointed
-                                        out, prevents some of the most
-                                        significant new ideas from fruition...
-                                        and keeps the "same old, same old" in
-                                        place. ThunderHouse addresses the many
-                                        facets of this dilemma.
-                                    </p>
-                                </div>
-
-                                <div className="think__item-image">
-                                    <img
-                                        src="/images/think-1.png"
-                                        alt="Think"
-                                    />
-                                </div>
-                            </div>
-                            {/* Single Item */}
-
-                            <div className="qoutes-box glossy__bg_quote">
-                                <h2>
-                                    “Our problem is not that we aim too high and
-                                    miss, But we aim too low and hit.”
-                                </h2>
-
-                                <p>- Aristotle 384-322 BC</p>
-                            </div>
-
-                            {/* Single Item */}
-                            <div className="think__item">
-                                <div className="think__item-content single__content-subhead">
-                                    <h3>What is Significant Innovation?</h3>
-                                    <p>
-                                        Each Organization should define
-                                        “Significance” in accordance with its
-                                        mission and objectives. For
-                                        ThunderHouse, “Significance” Is defined
-                                        as improving the Quality of Life of
-                                        individuals and enough individuals to
-                                        impact the Quality of Life of society.
-                                    </p>
-                                </div>
-
-                                <div className="think__item-image">
-                                    <img
-                                        src="/images/think-2.png"
-                                        alt="Think"
-                                    />
-                                </div>
-                            </div>
-                            {/* Single Item */}
-
-                            {/* Single Item */}
-                            <div className="think__item">
-                                <div className="think__item-content single__content-subhead">
-                                    <h3>What is Sustainable Growth?</h3>
-                                    <p>
-                                        The term “Sustainable” is most
-                                        frequently associated with ESG
-                                        initiatives: Social Environmental,
-                                        Social and Governance. ThunderHouse
-                                        defines “sustainability” in a broader
-                                        context: the ability of an organization
-                                        to sustain itself over time financially
-                                        and security-wise as as well as in terms
-                                        of ESG. As Innovations fuel sustainable
-                                        growth, Profitability generates the
-                                        investment necessary for Innovation. And
-                                        today, without addressing Security,
-                                        organizations are increasingly at risk.
-                                    </p>
-                                </div>
-
-                                <div className="think__item-image">
-                                    <img
-                                        src="/images/think-3.png"
-                                        alt="Think"
-                                    />
-                                </div>
-                            </div>
-                            {/* Single Item */}
-                        </div>
-
-                        
-                    </div>
-                    {/* Single Content */}
-                </div>
+            <div className="single__content-subhead">
+              <p>
+                ThunderHouse focuses on Innovation because Innovation fuels
+                growth…and significant innovations fuel significant, sustainable
+                growth.
+              </p>
             </div>
-        </section>
-    );
+
+            <div className="glossy__heading glossy__bg_quote">
+              <p>
+                Innovation (ɪnəˈveɪʃən/) n. A better way to satisfy needs and
+                wants
+              </p>
+            </div>
+
+            <div className="think__items">
+              {/* Single Item */}
+              <div className="think__item">
+                <div className="think__item-content single__content-subhead">
+                  <h3>What is Innovation?</h3>
+                  <p>
+                    The very structure of established organizations, the
+                    operations, systems and procedures, that keep companies on
+                    track today become barriers to innovations that fuel the
+                    growth of these businesses tomorrow. The "Innovator's
+                    Dilemma," as Harvard professor Clayton Christensen pointed
+                    out, prevents some of the most significant new ideas from
+                    fruition... and keeps the "same old, same old" in place.
+                    ThunderHouse addresses the many facets of this dilemma.
+                  </p>
+                </div>
+
+                <div className="think__item-image">
+                  <img src="/images/think-1.jpg" alt="Think" />
+                </div>
+              </div>
+              {/* Single Item */}
+
+              <div className="qoutes-box glossy__bg_quote">
+                <h2>
+                  “Our problem is not that we aim too high and miss, But we aim
+                  too low and hit.”
+                </h2>
+
+                <p>- Aristotle 384-322 BC</p>
+              </div>
+
+              {/* Single Item */}
+              <div className="think__item">
+                <div className="think__item-content single__content-subhead">
+                  <h3>What is Significant Innovation?</h3>
+                  <p>
+                    Each Organization should define “Significance” in accordance
+                    with its mission and objectives. For ThunderHouse,
+                    “Significance” Is defined as improving the Quality of Life
+                    of individuals and enough individuals to impact the Quality
+                    of Life of society.
+                  </p>
+                </div>
+
+                <div className="think__item-image">
+                  <img src="/images/think-2.jpg" alt="Think" />
+                </div>
+              </div>
+              {/* Single Item */}
+
+              {/* Single Item */}
+              <div className="think__item">
+                <div className="think__item-content single__content-subhead">
+                  <h3>What is Sustainable Growth?</h3>
+                  <p>
+                    The term “Sustainable” is most frequently associated with
+                    ESG initiatives: Social Environmental, Social and
+                    Governance. ThunderHouse defines “sustainability” in a
+                    broader context: the ability of an organization to sustain
+                    itself over time financially and security-wise as as well as
+                    in terms of ESG. As Innovations fuel sustainable growth,
+                    Profitability generates the investment necessary for
+                    Innovation. And today, without addressing Security,
+                    organizations are increasingly at risk.
+                  </p>
+                </div>
+
+                <div className="think__item-image">
+                  <img src="/images/think-3.jpg" alt="Think" />
+                </div>
+              </div>
+              {/* Single Item */}
+            </div>
+          </div>
+          {/* Single Content */}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Think;
--- app/components/work/work.js
+++ app/components/work/work.js
@@ -7,156 +7,132 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const Work = () => {
-    useEffect(() => {
-        const innerContents = document.querySelectorAll(".other__fade-in");
+  useEffect(() => {
+    const innerContents = document.querySelectorAll(".other__fade-in");
 
-        gsap.to(".other__hero-area", { opacity: 1, duration: 3 });
+    gsap.to(".other__hero-area", { opacity: 1, duration: 3 });
 
-        innerContents.forEach((content) => {
-            const innerElements = content.querySelectorAll(
-                "h1, h2, h3, h4, h5, p, .single__content-image, .think__item-image"
-            );
+    innerContents.forEach((content) => {
+      const innerElements = content.querySelectorAll(
+        "h1, h2, h3, h4, h5, p, .single__content-image, .think__item-image"
+      );
 
-            innerElements.forEach((element) => {
-                gsap.from(element, {
-                    opacity: 0,
-                    filter: "blur(6px)",
-                    stagger: 0.25,
-                    ease: "power2.out",
-                    duration: 0.8,
-                    scrollTrigger: {
-                        trigger: element,
-                        start: "top 80%",
-                        toggleActions: "play none none none",
-                    },
-                });
-            });
+      innerElements.forEach((element) => {
+        gsap.from(element, {
+          opacity: 0,
+          filter: "blur(6px)",
+          stagger: 0.25,
+          ease: "power2.out",
+          duration: 0.8,
+          scrollTrigger: {
+            trigger: element,
+            start: "top 80%",
+            toggleActions: "play none none none",
+          },
         });
-    }, []);
+      });
+    });
+  }, []);
 
-    return (
-        <section className="other__hero-area">
-            <div className="other__hero-bg"></div>
-            <div className="other__hero-bg other__white-bg"></div>
+  return (
+    <section className="other__hero-area">
+      <div className="other__hero-bg"></div>
+      <div className="other__hero-bg other__white-bg"></div>
 
-            <div className="container">
-                <div className="other__hero">
-                    {/* Single Content */}
-                    <div className="single__content other__fade-in">
-                        <h1>How We Work</h1>
+      <div className="container">
+        <div className="other__hero">
+          {/* Single Content */}
+          <div className="single__content other__fade-in">
+            <h1>How We Work</h1>
 
-                        <div className="single__content-subhead">
-                            <p>
-                                ThunderHouse begins its work at the frontend of
-                                the Communications Process: It then helps
-                                attract best-in-class talent with
-                                market-specific expertise to implement
-                                successful marketing programs.{" "}
-                            </p>
+            <div className="single__content-subhead">
+              <p>
+                ThunderHouse begins its work at the frontend of the
+                Communications Process: It then helps attract best-in-class
+                talent with market-specific expertise to implement successful
+                marketing programs.{" "}
+              </p>
 
-                            <p>
-                                At each step in a well-defined, collaborative
-                                process, ThunderHouse seeks a{" "}
-                                <i>faster, more cost effective track</i>.{" "}
-                            </p>
-                        </div>
-                        
-
-                        <div className="think__items">
-                            {/* Single Item */}
-                            <div className="think__item">
-                                <div className="think__item-content single__content-subhead">
-                                    <h3>The Three Dimensional Approach</h3>
-                                    <p>
-                                        ThunderHouse views the innovator's challenge in
-                                    three dimensionsl:{" "}
-                                    <b>
-                                        The Product, The Audience and The
-                                        Marketplace. &nbsp;
-                                    </b>
-                                    With a clear vision of success in hand,
-                                    ThunderHouse helps Clients formulate
-                                    positionings, strategies and action plans. It
-                                    uses proven, visually-engaging tools to
-                                    facilitate collaboration, to sort through a
-                                    multiplicity of possibilities and expedite
-                                    decision making.{" "}
-                                    </p>
-                                </div>
-
-                                <div className="think__item-image">
-                                    <img
-                                        src="/images/think-1.png"
-                                        alt="Think"
-                                    />
-                                </div>
-                            </div>
-                            {/* Single Item */}
-
-
-                            {/* Single Item */}
-                            <div className="think__item">
-                                <div className="think__item-content single__content-subhead">
-                                    <h3>"Best of Class" Resourcing</h3>
-                                    <p>
-                                    ThunderHouse will not be re-inventing resources
-                                that already exist, but rather recognizing and
-                                securing "best-of-class" talent, with
-                                market-specific experience, whether the talent
-                                is affiliated with an existing entity and
-                                working independently. The world doesn't need
-                                another branding company, another advertising
-                                agency, another search marketing company or
-                                another investor relations or lobbying firm.
-                                ThunderHouse is experienced in identifying the
-                                best talent, negotiating terms of engagement,
-                                and most importantly facilitating collaboration.{" "}
-                                    </p>
-                                </div>
-
-                                <div className="think__item-image">
-                                    <img
-                                        src="/images/think-2.png"
-                                        alt="Think"
-                                    />
-                                </div>
-                            </div>
-                            {/* Single Item */}
-
-                            {/* Single Item */}
-                            <div className="think__item">
-                                <div className="think__item-content single__content-subhead">
-                                    <h3>Compensation and Rewards for Success</h3>
-                                    <p>
-                                        ThunderHouse resources will be compensated at
-                                market rates for their services to develop
-                                strategies and implement Action Plans. However,
-                                ThunderHouse principals will be rewarded
-                                primarily on the basis of success in achieving
-                                agreed-upon objectives. The specific performance
-                                compensation will be determined on an
-                                innovation-specific basis. "Best-of-Class"
-                                outside resources will be compensated fairly at
-                                market rates, without any ThunderHouse markup.{" "}
-                                    </p>
-                                </div>
-
-                                <div className="think__item-image">
-                                    <img
-                                        src="/images/think-3.png"
-                                        alt="Think"
-                                    />
-                                </div>
-                            </div>
-                            {/* Single Item */}
-                        </div>
-
-                       
-                    </div>
-                </div>
+              <p>
+                At each step in a well-defined, collaborative process,
+                ThunderHouse seeks a <i>faster, more cost effective track</i>.{" "}
+              </p>
             </div>
-        </section>
-    );
+
+            <div className="think__items">
+              {/* Single Item */}
+              <div className="think__item">
+                <div className="think__item-content single__content-subhead">
+                  <h3>The Three Dimensional Approach</h3>
+                  <p>
+                    ThunderHouse views the innovator's challenge in three
+                    dimensionsl:{" "}
+                    <b>The Product, The Audience and The Marketplace. &nbsp;</b>
+                    With a clear vision of success in hand, ThunderHouse helps
+                    Clients formulate positionings, strategies and action plans.
+                    It uses proven, visually-engaging tools to facilitate
+                    collaboration, to sort through a multiplicity of
+                    possibilities and expedite decision making.{" "}
+                  </p>
+                </div>
+
+                <div className="think__item-image">
+                  <img src="/images/think-1.jpg" alt="Think" />
+                </div>
+              </div>
+              {/* Single Item */}
+
+              {/* Single Item */}
+              <div className="think__item">
+                <div className="think__item-content single__content-subhead">
+                  <h3>"Best of Class" Resourcing</h3>
+                  <p>
+                    ThunderHouse will not be re-inventing resources that already
+                    exist, but rather recognizing and securing "best-of-class"
+                    talent, with market-specific experience, whether the talent
+                    is affiliated with an existing entity and working
+                    independently. The world doesn't need another branding
+                    company, another advertising agency, another search
+                    marketing company or another investor relations or lobbying
+                    firm. ThunderHouse is experienced in identifying the best
+                    talent, negotiating terms of engagement, and most
+                    importantly facilitating collaboration.{" "}
+                  </p>
+                </div>
+
+                <div className="think__item-image">
+                  <img src="/images/think-2.jpg" alt="Think" />
+                </div>
+              </div>
+              {/* Single Item */}
+
+              {/* Single Item */}
+              <div className="think__item">
+                <div className="think__item-content single__content-subhead">
+                  <h3>Compensation and Rewards for Success</h3>
+                  <p>
+                    ThunderHouse resources will be compensated at market rates
+                    for their services to develop strategies and implement
+                    Action Plans. However, ThunderHouse principals will be
+                    rewarded primarily on the basis of success in achieving
+                    agreed-upon objectives. The specific performance
+                    compensation will be determined on an innovation-specific
+                    basis. "Best-of-Class" outside resources will be compensated
+                    fairly at market rates, without any ThunderHouse markup.{" "}
+                  </p>
+                </div>
+
+                <div className="think__item-image">
+                  <img src="/images/think-3.jpg" alt="Think" />
+                </div>
+              </div>
+              {/* Single Item */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Work;

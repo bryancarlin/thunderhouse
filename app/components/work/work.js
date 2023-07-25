45cdc380b720020d18fec3288a6d063a70d291d4
Bryan Carlin <bryancarlin@Bryans-MBP.lan>
2023-07-25 08:42:57 -0400

    first commit

--- app/components/work/work.js
+++ app/components/work/work.js
@@ -63,70 +63,55 @@ const Work = () => {
               {/* Single Item */}
               <div className="think__item">
                 <div className="think__item-content single__content-subhead">
-                  <h3>The Three Dimensional Approach</h3>
-                  <p>
-                    ThunderHouse views the innovator's challenge in three
-                    dimensions:{" "}
-                    <b>The Product, The Audience and The Marketplace. &nbsp;</b>
-                    With a clear vision of success in hand, ThunderHouse helps
-                    Clients formulate positionings, strategies and action plans.
-                    It uses proven, visually-engaging tools to facilitate
-                    collaboration, to sort through a multiplicity of
-                    possibilities and expedite decision making.{" "}
-                  </p>
+                  <h3>Begin Work At the Frontend of Communications</h3>
+                  <p> ThunderHouse starts with Market Understanding whether that market is for your product, candidate or idea.   Understanding requires knowledge of 1) Product/Candidate/issues, 2) the Audience(s) and 3) the behavioral environment often called the Marketplace.  Understanding, then naturally leads to an articulation of a Vision of Success and Basic Next Steps.   Once that Vision is clear and agreed to, the other elements of Strategy Development are addressed to achieve that Vision of Success.   Strategy forms the necessary foundation and guidance for Tactical implementation.</p>
+                  <p>ThunderHouse uses proven, visually engaging tools to facilitate collaboration, to sort through a multiplicity of possibilities and to expedite decision making.  For more detail on the frontend process and the Spectrum tool click here. </p>
                 </div>
 
                 <div className="think__item-image">
-                  <img src="/images/work1.jpg" alt="Think" />
+                  <img src="/images/spectrum.jpg" alt="Spectrum" />
                 </div>
               </div>
               {/* Single Item */}
 
-              {/* Single Item */}
-              <div className="think__item">
-                <div className="think__item-content single__content-subhead">
-                  <h3>"Best of Class" Resourcing</h3>
-                  <p>
-                    ThunderHouse will not be re-inventing resources that already
-                    exist, but rather recognizing and securing "best-of-class"
-                    talent, with market-specific experience, whether the talent
-                    is affiliated with an existing entity and working
-                    independently. The world doesn't need another branding
-                    company, another advertising agency, another search
-                    marketing company or another investor relations or lobbying
-                    firm. ThunderHouse is experienced in identifying the best
-                    talent, negotiating terms of engagement, and most
-                    importantly facilitating collaboration.{" "}
-                  </p>
-                </div>
+              <div className="features__cards" id="nomargin">
+                    {/* Single Card */}
+                    <div className="features__card single__content-subhead glossy__bg" id="tallcard">
+                        <h3>"Best of Class" Resourcing</h3>
 
-                <div className="think__item-image">
-                  <img src="/images/work2.jpg" alt="Think" />
-                </div>
-              </div>
-              {/* Single Item */}
+                        <p>
+                        ThunderHouse will not be re-inventing resources that already
+                        exist, but rather recognizing and securing "best-of-class"
+                        talent, with market-specific experience, whether the talent
+                        is affiliated with an existing entity and working
+                        independently. The world doesn't need another branding
+                        company, another advertising agency, another search
+                        marketing company or another investor relations or lobbying
+                        firm. ThunderHouse is experienced in identifying the best
+                        talent, negotiating terms of engagement, and most
+                        importantly facilitating collaboration.
+                        </p>
+                    </div>
+                    {/* Single Card */}
+                    {/* Single Card */}
+                    <div className="features__card single__content-subhead glossy__bg" id="tallcard">
+                        <h3>Compensation & Rewards for Success</h3>
 
-              {/* Single Item */}
-              <div className="think__item">
-                <div className="think__item-content single__content-subhead">
-                  <h3>Compensation and Rewards for Success</h3>
-                  <p>
-                    ThunderHouse resources will be compensated at market rates
+                        <p>
+                        ThunderHouse resources will be compensated at market rates
                     for their services to develop strategies and implement
                     Action Plans. However, ThunderHouse principals will be
                     rewarded primarily on the basis of success in achieving
                     agreed-upon objectives. The specific performance
                     compensation will be determined on an innovation-specific
                     basis. "Best-of-Class" outside resources will be compensated
-                    fairly at market rates, without any ThunderHouse markup.{" "}
-                  </p>
-                </div>
+                    fairly at market rates, without any ThunderHouse markup.
+                        </p>
+                    </div>
 
-                <div className="think__item-image">
-                  <img src="/images/work3.jpg" alt="Think" />
-                </div>
               </div>
-              {/* Single Item */}
+
+             
             </div>
           </div>
         </div>
